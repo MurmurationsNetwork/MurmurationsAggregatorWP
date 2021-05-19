8560ed95be48c076efa3d260dbd2db9a72bd7fbb
AM <adam@photosynth.ca>
2021-05-19 16:41:29 +0200

    Update templates for new data structure.

--- templates/map_node_popup.php
+++ templates/map_node_popup.php
@@ -1,5 +1,5 @@
 <?php
-$popup_str = "<b>".$data->murmurations['name']."</b><br><p>".$data->murmurations['tagline']."</p><div class=\"murmurations-node-mission\">".$data->murmurations['mission']."</div><div class=\"murmurations-node-org-types\">".$data->murmurations['nodeTypes']."</div><a class=\"murmurations-node-url\" href=\"".$data->murmurations['url']."\">".$data->murmurations['url']."</a>";
+$popup_str = "<b>".$data['name']."</b><br><p>". wp_trim_words($data['description'],60,"...")."</p>";
 
 $popup_str = addslashes(str_replace(array("\r", "\n"),' ', $popup_str));
 
