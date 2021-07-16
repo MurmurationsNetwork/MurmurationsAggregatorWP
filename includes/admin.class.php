07eebd014d0417de7c5d1c6fd01c2a8e9ec2e8f3
AM <adam@photosynth.ca>
2021-07-16 15:00:36 +0200

    Styles for RJSF form

--- includes/admin.class.php
+++ includes/admin.class.php
@@ -86,10 +86,6 @@ class Admin {
     ?>
 <div id="murmagg-admin-form"></div>
 
-<!-- link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" -->
-
 <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-jsonschema-form/dist/react-jsonschema-form.js"></script>
