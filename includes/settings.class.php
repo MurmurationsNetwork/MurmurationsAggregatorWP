dc454eef47c49121f9b90f15151052dc93991417
AM <adam@photosynth.ca>
2021-07-20 16:12:01 +0200

    Move settings methods to settings class

--- includes/settings.class.php
+++ includes/settings.class.php
@@ -25,4 +25,26 @@ class Settings {
 		self::$settings[ $var ] = $value;
 	}
 
+  public static function load() {
+    self::$settings = get_option( 'murmurations_aggregator_settings' );
+  }
+
+  public static function save() {
+    return update_option( 'murmurations_aggregator_settings', self::$settings );
+  }
+
+  public static function get_schema_json(){
+    return file_get_contents( MURMAG_ROOT_PATH . 'admin_fields_jschema.json' );
+  }
+
+  public static function get_schema_array(){
+    return json_decode( self::get_schema_json(), true );
+  }
+
+  public static function get_fields(){
+    $admin_schema = self::get_schema_array();
+
+    return $admin_schema['properties'];
+  }
+
 }
