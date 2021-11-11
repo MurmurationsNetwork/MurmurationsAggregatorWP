1dda257713c5dcf2b3c04aabbc887274bdeee21a
AM <adam@photosynth.ca>
2021-11-11 16:30:00 +0100

    Update archive node template with image output

--- templates/archive-murmurations_node.php
+++ templates/archive-murmurations_node.php
@@ -23,49 +23,63 @@ get_header();
     ?>
 
     <article id="<?= $post->ID ?>" class="post murmurations-node post-<?=$post->ID?> type-murmurations_node status-publish format-standard hentry">
-      <header class="entry-header has-text-align-center">
-    <?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
-      </header>
-      <div class="post-meta-wrapper">
-    <?php if ( $data['url'] ) : ?>
-      <div class="murmurations-node-url"><a href="<?php echo esc_url( $data['url'] ); ?>"><?php echo  esc_url( $data['url'] ); ?></a></div>
-    <?php endif; ?>
-      </div>
-      <div class="entry-content">
-        <?php
-        the_excerpt();
-        if ( Murmurations\Aggregator\Config::get('node_single') ) {
-          if ( Murmurations\Aggregator\Config::get('node_single_url_field') ) {
-            $href   = $data[ Murmurations\Aggregator\Config::get('node_single_url_field') ];
-            $target = ' target="_blank"';
-          } else {
-            $href = $data['guid'];
-          }
-          ?><p><a href="<?php echo esc_url( $href ); ?>" <?php echo $target; ?>>read more</a></p>
+
+      <div class="murmurations-node-content">
+        <?php if ( isset($data['image'][0]['url']) ) : ?>
+          <div class="murmurations-node-image"><img src="<?php echo esc_url( $data['image'][0]['url'] ); ?>"></div>
+        <?php endif; ?>
+        <div class="murmurations-node-text">
+          <header class="entry-header has-text-align-center">
+        <?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+          </header>
+
+          <div class="post-meta-wrapper">
+        <?php if ( $data['url'] ) : ?>
+          <div class="murmurations-node-url"><a href="<?php echo esc_url( $data['url'] ); ?>"><?php echo  esc_url( $data['url'] ); ?></a></div>
+        <?php endif; ?>
+          </div>
+
+
           <?php
-        }
+          the_excerpt();
+          if ( Murmurations\Aggregator\Config::get('node_single') ) {
+            if ( Murmurations\Aggregator\Config::get('node_single_url_field') ) {
+              $single_url_field = Murmurations\Aggregator\Config::get('node_single_url_field');
+              if ( trim( $single_url_field ) != '' ){
+                $href   = $data[ Murmurations\Aggregator\Config::get('node_single_url_field') ];
+                $target = ' target="_blank"';
+              } else {
+                $href = $data['guid'];
+              }
+            } else {
+              $href = $data['guid'];
+            }
+            ?><p><a href="<?php echo esc_url( $href ); ?>" <?php echo $target; ?>>read more</a></p>
+            <?php
+          }
+          ?>
 
-      ?>
+          <?php if ( $data['location'] ) : ?>
+          <div class="murmurations-node-location">
+            <?php
+
+            $place_components = array();
+
+            if ( $data['location']['locality'] ) {
+              $place_components[] = $data['location']['locality'];
+            }
+
+            if ( $data['location']['region'] ) {
+              $place_components[] = $data['location']['region'];
+            }
+
+            echo esc_html( join( ', ', $place_components ) );
+
+            ?>
+          </div>
+        <?php endif; ?>
       </div>
-      <?php if ( $data['location'] ) : ?>
-      <div class="murmurations-node-location">
-        <?php
-
-        $place_components = array();
-
-        if ( $data['location']['locality'] ) {
-          $place_components[] = $data['location']['locality'];
-        }
-
-        if ( $data['location']['region'] ) {
-          $place_components[] = $data['location']['region'];
-        }
-
-        echo esc_html( join( ', ', $place_components ) );
-
-        ?>
-      </div>
-    <?php endif; ?>
+    </div>
   </article>
 	<?php endwhile; ?>
 <?php
