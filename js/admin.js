cd6c72e523bd30c9306091772ea95f833f34340c
AdamM <adam@photosynthesis.ca>
2022-03-07 13:48:33 +0100

    Use recursion and async ajax for JS node update control (fixes #10)

--- js/admin.js
+++ js/admin.js
@@ -26,6 +26,69 @@ function viewLocalSchema(){
   });
 }
 
+function recursiveNodeUpdate(nodes, index){
+
+  console.log("REcursive node update called");
+  console.log(nodes);
+  console.log(index);
+
+  var node = nodes[index];
+
+  var data = {
+    'action': 'update_node',
+    'profile_url' : node.profile_url,
+    'index_options' : node.index_options
+  };
+
+  node_update_log("Fetching node " + (index + 1).toString() + " of " + nodes.length.toString());
+
+  stats.queried += 1;
+
+  jQuery.ajax({
+    type: "POST",
+    url: ajaxurl,
+    data: data,
+    success: function(response) {
+
+      console.log(response);
+
+      if(response.status.toString() == 'success'){
+        stats.success += 1;
+      } else {
+        stats.failed += 1;
+      }
+
+      for (const message of response.messages){
+        node_update_log(message.message.toString());
+      }
+      node_update_log("*** " + response.status.toString() + " *** \n");
+
+      if(nodes.length > (index + 1)){
+        recursiveNodeUpdate(nodes,(index + 1))
+      }else{
+
+        var data = {
+          'action': 'set_update_time'
+        };
+
+        jQuery.post(ajaxurl, data, function(response) {
+          if(response.status.toString() == 'success'){
+            node_update_log("Set update time");
+          } else {
+            node_update_log("Failed to set update time!");
+          }
+        });
+
+        node_update_log("Nodes queried: " + stats.queried.toString() );
+        node_update_log("Nodes failed: " + stats.failed.toString() );
+        node_update_log("Nodes saved: " + stats.success.toString() );
+
+      }
+    }
+  });
+
+}
+
 function ajaxUpdateNodes(){
 
 	logContainer = document.getElementById('murmagg-admin-form-log-container');
@@ -54,60 +117,9 @@ function ajaxUpdateNodes(){
         queried : 0
       };
 
-      node_update_log("Fetching " + response.nodes.length.toString() + " nodes...");
+      node_update_log("Fetching " + response.nodes.length.toString() + " nodes...\n");
 
-      for (const node of response.nodes){
-
-        var data = {
-          'action': 'update_node',
-          'profile_url' : node.profile_url,
-          'index_options' : node.index_options
-        };
-
-        node_update_log("Fetching " + node.profile_url.toString());
-
-        stats.queried += 1;
-
-        jQuery.ajax({
-          type: "POST",
-          url: ajaxurl,
-          data: data,
-          async : false,
-          success: function(response) {
-
-            console.log(response);
-
-            if(response.status.toString() == 'success'){
-              stats.success += 1;
-            } else {
-              stats.failed += 1;
-            }
-
-            for (const message of response.messages){
-              node_update_log(message.message.toString());
-            }
-            node_update_log("*** " + response.status.toString() + " ***");
-          }
-        });
-
-      }
-
-      var data = {
-    	  'action': 'set_update_time'
-    	};
-
-    	jQuery.post(ajaxurl, data, function(response) {
-        if(response.status.toString() == 'success'){
-          node_update_log("Set update time");
-        } else {
-          node_update_log("Failed to set update time!");
-        }
-      });
-
-      node_update_log("\nNodes queried: " + stats.queried.toString() );
-      node_update_log("Nodes failed: " + stats.failed.toString() );
-      node_update_log("Nodes saved: " + stats.success.toString() );
-
+      recursiveNodeUpdate(response.nodes, 0);
 
     } else {
       node_update_log("No nodes found at index");
