f3147b2ca62804ddbfe41b660c30269728bf9028
TingSyuanWang <vincent@fishboneapps.com>
2023-10-05 07:29:04 -0400

    fix: fix uninstall did not drop custom nodes table
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-aggregator-uninstall.php
+++ admin/classes/class-murmurations-aggregator-uninstall.php
@@ -1,13 +1,18 @@
 <?php
 
-if ( ! class_exists('Murmurations_Node_Uninstall') ) {
+if ( ! class_exists( 'Murmurations_Node_Uninstall' ) ) {
 	class Murmurations_Node_Uninstall {
 		public static function uninstall() {
 			global $wpdb;
-			$table_name = $wpdb->prefix . MURMURATIONS_AGGREGATOR_TABLE;
+			$table_name      = $wpdb->prefix . MURMURATIONS_AGGREGATOR_TABLE;
+			$node_table_name = $wpdb->prefix . MURMURATIONS_NODE_TABLE;
 
-			if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") === $table_name) {
-				$wpdb->query("DROP TABLE IF EXISTS $table_name");
+			if ( $wpdb->get_var( "SHOW TABLES LIKE '$node_table_name'" ) === $node_table_name ) {
+				$wpdb->query( "DROP TABLE IF EXISTS $node_table_name" );
+			}
+
+			if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) === $table_name ) {
+				$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 			}
 		}
 	}
