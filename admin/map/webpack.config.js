b8f92b7bcae3fb4a38def5dcc12149c639c3d478
TingSyuanWang <vincent@fishboneapps.com>
2023-10-16 15:45:54 -0400

    fix: fix webpack config
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/map/webpack.config.js
+++ admin/map/webpack.config.js
@@ -1,41 +1,43 @@
 const path = require('path');
 
-let publicPath
-if (process.env.NODE_ENV === 'production') {
-  publicPath = '/wp-content/plugins/MurmurationsAggregatorWP/admin/assets/map/'
-} else {
-  publicPath = '/wp-content/plugins/murmurations-aggregator/admin/assets/map/'
-}
+module.exports = (env, argv) => {
+  let publicPath
+  if (argv.mode === 'production') {
+    publicPath = '/wp-content/plugins/MurmurationsAggregatorWP/admin/assets/map/'
+  } else {
+    publicPath = '/wp-content/plugins/murmurations-aggregator/admin/assets/map/'
+  }
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, '../assets/map'),
-    filename: 'index.js',
-    publicPath: publicPath,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.png$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name]-[hash][ext]',
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, '../assets/map'),
+      filename: 'index.js',
+      publicPath: publicPath,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
         },
-      },
-    ],
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [ '.js', '.jsx' ],
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.png$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name]-[hash][ext]',
+          },
+        },
+      ],
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    }
   }
 };
