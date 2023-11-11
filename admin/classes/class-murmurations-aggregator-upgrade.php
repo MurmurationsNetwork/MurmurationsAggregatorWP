ecea4153846574bb5f5ee7b90258bbc9442e0ef7
TingSyuanWang <vincent@fishboneapps.com>
2023-11-11 13:52:13 -0500

    fix: fix upgrade function
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-aggregator-upgrade.php
+++ admin/classes/class-murmurations-aggregator-upgrade.php
@@ -3,29 +3,31 @@
 if ( ! class_exists( 'Murmurations_Aggregator_Upgrade' ) ) {
 	class Murmurations_Aggregator_Upgrade {
 		public function __construct() {
-			add_action( 'upgrade_process_complete', array( $this, 'upgrade_db_check' ) );
+			add_action( 'upgrader_process_complete', array( $this, 'upgrade_db_check' ) );
 		}
 
-		public static function upgrade_db_check(): void {
-			// uncomment the following line to upgrade the db
-			$current_version = get_option( 'murmurations_aggregator_version' );
+		public static function upgrade_db_check( $options ): void {
+			if ( $options['action'] == 'update' && $options['type'] == 'plugin' ) {
+				// uncomment the following line to upgrade the db
+				$current_version = get_option( 'murmurations_aggregator_version' );
 
-			$new_version = MURMURATIONS_AGGREGATOR_VERSION;
+				$new_version = MURMURATIONS_AGGREGATOR_VERSION;
 
-			if ( version_compare( $current_version, $new_version, '<' ) ) {
-				// update db
-				global $wpdb;
-				$table_name = $wpdb->prefix . MURMURATIONS_AGGREGATOR_NODE_TABLE;
+				if ( version_compare( $current_version, $new_version, '<' ) ) {
+					// update db
+					global $wpdb;
+					$table_name = $wpdb->prefix . MURMURATIONS_AGGREGATOR_NODE_TABLE;
 
-				$sql = "ALTER TABLE $table_name
+					$sql = "ALTER TABLE $table_name
                         ADD COLUMN is_available BOOLEAN NOT NULL DEFAULT 0,
                         ADD COLUMN unavailable_message VARCHAR(255) DEFAULT NULL";
 
-				$wpdb->query( $sql );
-			}
+					$wpdb->query( $sql );
+				}
 
-			// update the plugin version in the database
-			update_option( 'murmurations_aggregator_version', $new_version );
+				// update the plugin version in the database
+				update_option( 'murmurations_aggregator_version', $new_version );
+			}
 		}
 	}
 }
