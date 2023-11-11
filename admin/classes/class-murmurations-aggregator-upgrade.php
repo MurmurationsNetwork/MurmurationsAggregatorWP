cf0ce042ad082d3301fdc036592264fe4fad5b69
TingSyuanWang <vincent@fishboneapps.com>
2023-11-11 00:50:01 -0500

    refactor: remove upgrade code
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-aggregator-upgrade.php
+++ admin/classes/class-murmurations-aggregator-upgrade.php
@@ -8,24 +8,24 @@ if ( ! class_exists( 'Murmurations_Aggregator_Upgrade' ) ) {
 
 		public static function upgrade_db_check(): void {
 			// uncomment the following line to upgrade the db
-			$current_version = get_option( 'murmurations_aggregator_version' );
-
-			$new_version = 'v1.0.0-beta.1';
-
-			if ( version_compare( $current_version, $new_version, '<' ) ) {
-				// update db
-				global $wpdb;
-				$table_name = $wpdb->prefix . MURMURATIONS_AGGREGATOR_NODE_TABLE;
-
-				$sql = "ALTER TABLE $table_name 
-                        ADD COLUMN is_available BOOLEAN NOT NULL DEFAULT 0, 
-                        ADD COLUMN unavailable_message VARCHAR(255) DEFAULT NULL";
-
-				$wpdb->query( $sql );
-
-				// update the plugin version in the database
-				update_option( 'murmurations_aggregator_version', $new_version );
-			}
+//			$current_version = get_option( 'murmurations_aggregator_version' );
+//
+//			$new_version = 'v1.0.0-beta.1';
+//
+//			if ( version_compare( $current_version, $new_version, '<' ) ) {
+//				// update db
+//				global $wpdb;
+//				$table_name = $wpdb->prefix . MURMURATIONS_AGGREGATOR_NODE_TABLE;
+//
+//				$sql = "ALTER TABLE $table_name
+//                        ADD COLUMN is_available BOOLEAN NOT NULL DEFAULT 0,
+//                        ADD COLUMN unavailable_message VARCHAR(255) DEFAULT NULL";
+//
+//				$wpdb->query( $sql );
+//
+//				// update the plugin version in the database
+//				update_option( 'murmurations_aggregator_version', $new_version );
+//			}
 		}
 	}
 }
