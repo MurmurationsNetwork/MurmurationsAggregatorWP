b798a4d0df5fd1f1689b3a21a73a6cc46665239e
Geoff Turk <geoff@ic3.dev>
2023-12-13 12:58:31 +0100

    fix: properly recurse through objects, fix array iteration

--- admin/map/src/utils/iterateObject.js
+++ admin/map/src/utils/iterateObject.js
@@ -1,13 +1,22 @@
 let keyValueMap = {}
 
 export function iterateObject(obj, currentKey = '') {
+  if (!currentKey) {
+    keyValueMap = {}
+  }
   for (const key in obj) {
     if (typeof obj[key] === 'object') {
-      iterateObject(obj[key], currentKey + key + '.')
-    } else if (Array.isArray(obj[key])) {
-      obj[key].forEach((item, index) => {
-        keyValueMap[`${currentKey}${key}[${index}]`] = item
-      })
+      if (Array.isArray(obj[key])) {
+        obj[key].forEach((item, index) => {
+          if (typeof item === 'object') {
+            iterateObject(item, `${currentKey}${key}[${index}].`)
+          } else {
+            keyValueMap[`${currentKey}${key}[${index}]`] = item
+          }
+        })
+      } else {
+        iterateObject(obj[key], `${currentKey}${key}.`)
+      }
     } else {
       keyValueMap[`${currentKey}${key}`] = obj[key]
     }
