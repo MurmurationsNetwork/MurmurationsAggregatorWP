17e683dbda109872af20d496cd321bff72c12125
TingSyuanWang <vincent@fishboneapps.com>
2023-12-26 00:16:30 -0500

    feat: add child themes for twentytwenty and twentytwentyfour
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ admin/views/twentytwenty-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+function my_child_theme_styles(): void {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'my_child_theme_styles' );
