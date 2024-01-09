a5cd3a09050dd6d669d08d353efa2dd21a7f81f0
TingSyuanWang <vincent@fishboneapps.com>
2024-01-09 15:27:13 -0500

    fix: use constant in deactivate and uninstall function
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- admin/classes/class-murmurations-aggregator-deactivation.php
+++ admin/classes/class-murmurations-aggregator-deactivation.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'Murmurations_Aggregator_Deactivation' ) ) {
 	class Murmurations_Aggregator_Deactivation {
 		public static function deactivate(): void {
 			// Unregister the post type, so the rules are no longer in memory.
-			unregister_post_type( 'murmurations_node' );
-			unregister_taxonomy( 'murmurations_node_tags' );
-			unregister_taxonomy( 'murmurations_node_categories' );
+			unregister_post_type( MURMURATIONS_AGGREGATOR_POST_TYPE );
+			unregister_taxonomy( MURMURATIONS_AGGREGATOR_TAG_TAXONOMY );
+			unregister_taxonomy( MURMURATIONS_AGGREGATOR_CATEGORY_TAXONOMY );
 			// Clear the permalinks to remove our post type's rules from the database.
 			flush_rewrite_rules();
 		}
--- admin/classes/class-murmurations-aggregator-uninstall.php
+++ admin/classes/class-murmurations-aggregator-uninstall.php
@@ -42,9 +42,9 @@ if ( ! class_exists( 'Murmurations_Aggregator_Uninstall' ) ) {
 			}
 
 			// Unregister the post type, so the rules are no longer in memory.
-			unregister_post_type( 'murmurations_node' );
-			unregister_taxonomy( 'murmurations_node_tags' );
-			unregister_taxonomy( 'murmurations_node_categories' );
+			unregister_post_type( MURMURATIONS_AGGREGATOR_POST_TYPE );
+			unregister_taxonomy( MURMURATIONS_AGGREGATOR_TAG_TAXONOMY );
+			unregister_taxonomy( MURMURATIONS_AGGREGATOR_CATEGORY_TAXONOMY );
 			// Clear the permalinks to remove our post type's rules from the database.
 			flush_rewrite_rules();
 
